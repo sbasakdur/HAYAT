6a0a79e2abb21edcf1811cad8520d32711a05b1c
Ensar Karamürteza <69035284+ensarkara13@users.noreply.github.com>
2020-11-13 11:35:28 +0300

    Update Layoutjs.js

--- HayaTeknolojiCRM.Web/Scripts/Layoutjs.js
+++ HayaTeknolojiCRM.Web/Scripts/Layoutjs.js
@@ -7,6 +7,7 @@
     "use strict";
 
     // Add active state to sidbar nav links
+   //Ensar was here
     var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
     $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function () {
         if (this.href === path) {
